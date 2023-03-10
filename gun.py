2e6433ae9aeecfe4d9990e8428a3b3ed2d4340c5
ubuntu <serg.main.post@gmail.com>
2023-03-10 17:21:45 +0200

    first lesson

---
+++ gun.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Gun():
+
+    def __init__(self, screen):
+        """gun init"""
+
+        self.screen = screen
+        self.image = pygame.image.load('images/gun.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def output(self):
+        """image gun"""
+        self.screen.blit(self.image, self.rect)
--- py_invaders_main.py
+++ py_invaders_main.py
@@ -1,7 +1,26 @@
 import pygame
 import sys
+from gun import Gun
 
 def run():
 
     pygame.init()
-    screen = pygame.display.set_mode(())
+    screen = pygame.display.set_mode((1280, 800))
+    pygame.display.set_caption("Python Invaders")
+    bg_color = (0, 0, 0)
+
+    gun = Gun(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        gun.output()
+        pygame.display.flip()
+
+
+
+run()
+
